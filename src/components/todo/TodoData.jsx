7ed1e67b5955d4c30d22bda11955103b2d02318f
Xuân hạ <hbui4733@gmail.com>
2024-08-22 18:30:16 +0700

    akira 33

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,9 @@
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+        <div> learningReact </div>
+        <div> Watching Youtube </div>
+        </div>
+    )
+}
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,9 @@
+const TodoNew = () => {
+    return (
+        <div className='todo-new'>
+        <input type="text" />
+        <button> ADD </button>
+      </div>
+    )
+}
+export default TodoNew;
