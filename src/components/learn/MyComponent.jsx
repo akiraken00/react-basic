b9191d4f04b4ba955b7f384d4707a5dab836fe06
Xuân hạ <hbui4733@gmail.com>
2024-08-22 15:12:25 +0700

    akira 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,11 +3,22 @@
 
 import './style.css'
 const MyComponent = () =>{
+    // const Aiko = "Akira 1" // string
+    // const Aiko = 25; //number
+    // const Aiko = true; // boolean
+    // const Aiko = undefined;
+    // const Aiko = null;
+    const Aiko = [1, 2, 3];
+    // const Aiko = {
+    //     name: "Akira",
+    //     age: 24
+    // };
     return (
     <>
-        <div>Akira</div>
+        <div> {JSON.stringify(Aiko)} Akira</div>
+        <div>{console.log("Aiko")}</div>
         <div className="child"
-        style={{borderRadius: "10px"}}
+            style={{borderRadius: "10px"}}
         >child</div>
     </>
     );
