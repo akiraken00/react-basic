4d3cc987c8ddb69ac62cf42522cd1a57ac3f826b
Xuân hạ <hbui4733@gmail.com>
2024-08-26 00:53:41 +0700

    akira 37

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,9 +1,18 @@
 const TodoData = (props) => {
+{
 // props là một object {}
     // name :"Aiko"
     // age: 25;
     // data: {}
-    
+}
+// vi du 1 
+//     const { name, age, data } = props;
+
+// vi du 2
+//   const name = props.name;
+//   const age = props.age;
+//   const data = props.data;
+
 
     console.log(">>> check props :", props)
     return (
