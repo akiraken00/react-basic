e47382294821137792ae24e59c6d9f003a8f8c1e
Xuân hạ <hbui4733@gmail.com>
2024-08-28 20:13:17 +0700

    akira 40

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,9 @@ import { useState } from 'react';
 const App = () => {
 
   const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React " },
-    { id: 2, name: "Watching Youtube" }
+    // { id: 1, name: "Learning React " },
+    // { id: 2, name: "Watching Youtube" }
   ])
-
-  const Akira = "Akira ken";
-  const age = 25;
-  const data = {
-    address: "hanoi",
-    country: "vietnam"
-  }
 
   const addNewTodo = (name) => {
     const newTodo = {
@@ -39,9 +32,6 @@ const App = () => {
         addNewTodo={addNewTodo}
       />
       <TodoData
-        name={Akira}
-        age={age}
-        data={data}
         todoList={todoList}
       />
       <div className='todo-image'>
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -11,7 +11,7 @@ const TodoNew = (props) => {
     // addNewTodo("eric") //fire
     const handleClick = () => {
         addNewTodo(valueInput)
-        setValueInput("");
+        setValueInput("akira");
     }
 
     const handleOnChange = (name) => {
